b6f73efef82099ef00c445cbb6b012bcad49f9d2
Jack-in <64306586+Jack-in@users.noreply.github.com>
2023-11-08 18:21:56 +0530

    Create rand.js

---
+++ rand.js
@@ -0,0 +1,66 @@
+<div class="bookmarks">
+    <div class="row1">
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image/">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+           <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+    </div>
+    <div class="row2">
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image/">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>
+      <div class="col">
+        <div class="round">
+          <img class="book-img" src="data:image">
+        </div>
+        <p class="icon-name">gmail</p>
+      </div>                                                                                              
+    </div>
+</div>
